d19bc167a2d4db3e7ce6a0ee4199fb1a28f7f67c
Simon Podhajsky <simon.podhajsky@gmail.com>
2014-11-01 11:56:43 -0400

    Created a scaffold for Dashboard controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   root to: 'visitors#index'
+
   devise_for :users
   resources :users
   resources :contacts
+
+  get 'dashboard' => 'dashboard#index'
+  get 'dashboard/index'
 end
