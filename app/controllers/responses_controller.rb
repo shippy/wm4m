4157aaa215efd649c636352f834b8558780a1912
Simon Podhajsky <simon.podhajsky@gmail.com>
2014-11-01 16:33:02 -0400

    Update to response API

--- app/controllers/responses_controller.rb
+++ app/controllers/responses_controller.rb
@@ -2,17 +2,24 @@ class ResponsesController < ApplicationController
   protect_from_forgery with: :null_session
 
   def add
+    # debug
     puts params
-    response = Response.new(response_params)
-    response.save
 
     # TODO: Authentication logic (is the pebble ID registered here?)
-    render status: 200, text: "Success (response saved)"
+
+    response = Response.new(response_params)
+    response.datetime = DateTime.now
+
+    if response.save
+      render status: 200, text: "Success (response saved)"
+    else
+      render status: 500, text: "Failure to save response"
+    end
   end
 
   private
 
     def response_params
-      params.require(:response).permit(:kind, :level, :datetime)
+      params.require(:response).permit(:kind, :level, :user)
     end
 end
