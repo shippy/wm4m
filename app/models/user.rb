15e6927b575968bc39c81e6ce6345fba5c40a4dd
Simon Podhajsky <simon.podhajsky@gmail.com>
2014-11-01 11:23:09 -0400

    Small fix to make strange arity error go away

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,9 @@
 class User < ActiveRecord::Base
+  has_many :contacts
+  has_many :responses
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
-
-  has_many :contacts, :responses
 end
