8ebf6fda1d747e9d49fdddf543e66077adfdb9ce
Jacqueline <jschen06@gmail.com>
2014-11-02 01:57:43 -0400

    Fixing Heroku deployment failure of d3/sprockets

--- Gemfile
+++ Gemfile
@@ -12,6 +12,7 @@ gem 'spring',        group: :development
 gem 'bootstrap-sass'
 gem 'devise'
 gem 'simple_form'
+gem 'd3-rails'
 group :development do
   gem 'better_errors'
   gem 'binding_of_caller', :platforms=>[:mri_21]
--- app/assets/javascripts/dashboard.js
+++ app/assets/javascripts/dashboard.js
@@ -1,3 +1,17 @@
+
+  WebFontConfig = {
+    google: { families: [ 'Quicksand:300,400:latin' ] }
+  };
+  (function() {
+    var wf = document.createElement('script');
+    wf.src = ('https:' == document.location.protocol ? 'https' : 'http') +
+      '://ajax.googleapis.com/ajax/libs/webfont/1/webfont.js';
+    wf.type = 'text/javascript';
+    wf.async = 'true';
+    var s = document.getElementsByTagName('script')[0];
+    s.parentNode.insertBefore(wf, s);
+  })();
+
 ;( function() {
   var lat = $("#foo").data("lat");
   var date1 = $("#datetime1").data("lat");
