843343f25f3c8d5d96e21f2897a1c7d03844a29e
Simon Podhajsky <simon.podhajsky@gmail.com>
2014-11-02 01:17:12 -0500

    Fix as per sprockets#347

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -9,16 +9,14 @@ Rails.application.config.assets.precompile += %w( dashboard.js)
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 # Rails.application.config.assets.precompile += %w( search.js )
 
-initializer 'setup_asset_pipeline', :group => :all  do |app|
-  # We don't want the default of everything that isn't js or css, because it pulls too many things in
-  app.config.assets.precompile.shift
+# We don't want the default of everything that isn't js or css, because it pulls too many things in
+Rails.application.config.assets.precompile.shift
 
-  # Explicitly register the extensions we are interested in compiling
-  app.config.assets.precompile.push(Proc.new do |path|
-    File.extname(path).in? [
-      '.html', '.erb', '.haml',                 # Templates
-      '.png',  '.gif', '.jpg', '.jpeg',         # Images
-      '.eot',  '.otf', '.svc', '.woff', '.ttf', # Fonts
-    ]
-  end)
-end
+# Explicitly register the extensions we are interested in compiling
+Rails.application.config.assets.precompile.push(Proc.new do |path|
+  File.extname(path).in? [
+    '.html', '.erb', '.haml',                 # Templates
+    '.png',  '.gif', '.jpg', '.jpeg',         # Images
+    '.eot',  '.otf', '.svc', '.woff', '.ttf', # Fonts
+  ]
+end)
