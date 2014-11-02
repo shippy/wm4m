8dd0c5a93c50fb919f432819c8b42b962c920897
Simon Podhajsky <simon.podhajsky@gmail.com>
2014-11-02 01:13:32 -0500

    Fix as per sprockets#347

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -8,3 +8,17 @@ Rails.application.config.assets.precompile += %w( dashboard.js)
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 # Rails.application.config.assets.precompile += %w( search.js )
+
+initializer 'setup_asset_pipeline', :group => :all  do |app|
+  # We don't want the default of everything that isn't js or css, because it pulls too many things in
+  app.config.assets.precompile.shift
+
+  # Explicitly register the extensions we are interested in compiling
+  app.config.assets.precompile.push(Proc.new do |path|
+    File.extname(path).in? [
+      '.html', '.erb', '.haml',                 # Templates
+      '.png',  '.gif', '.jpg', '.jpeg',         # Images
+      '.eot',  '.otf', '.svc', '.woff', '.ttf', # Fonts
+    ]
+  end)
+end
