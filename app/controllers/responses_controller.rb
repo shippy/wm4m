2aec8e6a33d512537a1d2e4a37a82ee2d1e3802a
Simon Podhajsky <simon.podhajsky@gmail.com>
2014-11-02 05:36:18 -0500

    Added basic algo into Response#add evaluation

--- app/controllers/responses_controller.rb
+++ app/controllers/responses_controller.rb
@@ -1,18 +1,13 @@
-class ResponsesController < ApplicationController
-  
 require 'contact_mailer'
-
-protect_from_forgery with: :null_session
+class ResponsesController < ApplicationController
+  protect_from_forgery with: :null_session
 
   def add
-
     # TODO: Authentication logic (is the pebble ID registered here?)
 
     response = Response.new(response_params)
     # response.user = User.find(response_params[:response][:user_id])
     response.datetime = DateTime.now
-    
-
     # debug
     puts params
     puts response_params
@@ -24,15 +19,13 @@ protect_from_forgery with: :null_session
       render status: 500, text: "Failure to save response"
     end
 
-    if ( ((Response.where(kind: "1", user:current_user).pluck(:level).last(5).sum / 5.0) > 1.5) &&
-    ((Response.where(kind: "2",user:current_user).pluck(:level).last(5).sum / 5.0) < 1.5) ) 
-	ContactMailer.depressing_email(current_user,current_user.contacts.first)	   
+    if ((Response.where(kind: "1", user: current_user).pluck(:level).last(5).sum / 5.0) > 1.5) &&
+        ((Response.where(kind: "2",user:current_user).pluck(:level).last(5).sum / 5.0) < 1.5)
+      ContactMailer.depressing_email(current_user, current_user.contacts.sample).deliver
     end
-
   end
 
   private
-
     def response_params
       params.require(:response).permit(:kind, :level, :user_id)
     end
