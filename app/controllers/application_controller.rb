f05268b518d99b073bd739e261a25a17b7c2c4e6
Simon Podhajsky <simon.podhajsky@gmail.com>
2014-11-02 02:50:44 -0500

    Fixing the post-sign-in check

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   def after_sign_in_path_for(resource_or_scope)
-    if params[:user]
+    if params[:user][:pebble_token]
       if URI.parse(request.referer).path == connect_pebble_sign_in_path(params[:user][:pebble_token])
         URI.parse(request.referer).path
       end
